1cde4be8ed54a979104b11652b3ad1f5ade830da
PinkieSwirl <1779256+PinkieSwirl@users.noreply.github.com>
2023-12-06 18:05:22 +0100

    Fix Kotlin version upgrade

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.21"
-    kotlin("plugin.serialization") version "1.9.20"
+    kotlin("plugin.serialization") version "1.9.21"
     id("io.gitlab.arturbosch.detekt") version "1.23.4"
 }
 
