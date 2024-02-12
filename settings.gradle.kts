ffe83a4c4d666fae14643dcff20a12428c0d10fb
PinkieSwirl <1779256+PinkieSwirl@users.noreply.github.com>
2024-02-12 16:38:05 +0100

    da update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,11 +2,11 @@ pluginManagement {
     plugins {
         val kotlinVersion: String by settings
         val detektVersion: String by settings
-        // val daVersion: String by settings
+        val daVersion: String by settings
         kotlin("jvm") version kotlinVersion
         kotlin("plugin.serialization") version kotlinVersion
         id("io.gitlab.arturbosch.detekt") version detektVersion
-        // id("com.autonomousapps.dependency-analysis") version daVersion
+        id("com.autonomousapps.dependency-analysis") version daVersion
     }
     repositories {
         mavenCentral()
